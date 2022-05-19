7e7ea2b0601c695c907bfb2d0af21210c0c7f37a
SinitsaSveta <sinitsa9111@gmail.com>
2022-05-19 13:32:53 +0300

    first commit

---
+++ Lesson1.py
@@ -0,0 +1,7 @@
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+if first * first == second:
+    print(f"Первое число {first} квадрат второго числа {second}")
+else:
+    print(F"Первое число {first} не квадрат второго числа {second}")
+   
